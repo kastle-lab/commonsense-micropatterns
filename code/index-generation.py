98fdfd3e5d7570397be625384a7162f52ed12d4c
bddave <dave.15@wright.edu>
2024-05-26 10:53:24 -0400

    Categories for Antrea

--- code/index-generation.py
+++ code/index-generation.py
@@ -11,10 +11,11 @@ pattern_path = "../csmodl/patterns"
 
 # Prefix Configurations
 name_space = "https://kastle-lab.org/"
+modl_namespace = "https://archive.org/services/purl/purl/modular_ontology_design_library#"
 opla_namespace = "https://ontologydesignpatterns.org/"
 
 pfs = {
-"": Namespace(f"{opla_namespace}"),
+"": Namespace(f"{modl_namespace}"),
 "kl-res": Namespace(f"{name_space}lod/resource/"),
 "kl-ont": Namespace(f"{name_space}lod/ontology/"),
 "rdf": RDF,
@@ -46,11 +47,13 @@ def generate_index():
 
     # Noun CSMODL Ontology 
     ## Minting URIs
-    noun_ontology = f"{opla_namespace}"
+    noun_ontology = f"{modl_namespace}"
     noun_onto_uri = Namespace(noun_ontology)[""]
     
     pattern_uri = pfs["opla"]["Pattern"]
-   
+    category_uri = pfs["opla"]["Category"]
+    graph.add( (category_uri, a, pfs["owl"]["Class"]) )
+
     ## Bind
     graph.add( (noun_onto_uri, a, OWL.Ontology) )
     graph.add( (noun_onto_uri, pfs["opla-core"]["hasPatternName"], Literal("Commonsense Modular Ontology Design Library")) )
@@ -62,6 +65,7 @@ def generate_index():
     graph.add( (pfs["opla"]["renderedSchemaDiagram"], pfs["rdfs"]["label"], Literal(f"Rendered Schema Diagram", lang="en")) )
     graph.add( (pfs["opla"]["htmlDocumentation"], a, pfs["owl"]["DatatypeProperty"]) )
     graph.add( (pfs["opla"]["htmlDocumentation"], pfs["rdfs"]["label"], Literal(f"Owl HTML Documentation", lang="en")) )
+    graph.add( (pfs["opla"]["categorization"], a, pfs["owl"]["DatatypeProperty"]) )
 
     graph.add( (pfs["opla"]["owlRepresentation"], pfs["rdfs"]["domain"], pattern_uri) )
     graph.add( (pfs["opla"]["owlRepresentation"], pfs["rdfs"]["range"], pfs["xsd"]["string"]) )
@@ -69,14 +73,8 @@ def generate_index():
     graph.add( (pfs["opla"]["renderedSchemaDiagram"], pfs["rdfs"]["range"], pfs["xsd"]["string"]) )
     graph.add( (pfs["opla"]["htmlDocumentation"], pfs["rdfs"]["domain"], pattern_uri) )
     graph.add( (pfs["opla"]["htmlDocumentation"], pfs["rdfs"]["range"], pfs["xsd"]["string"]) )
-
-
-    #Add categorization
-
-    graph.add( (pfs["opla"]["categorization"], a, pfs["owl"]["DatatypeProperty"]) )
-    graph.add( (pfs["opla"]["categorization"], pfs["rdfs"]["label"], Literal(f"Owl Representation", lang="en")) )
-    graph.add((pfs["opla"]["categorization"],pfs["rdfs"]["domain"],pattern_uri))
-    graph.add( (pfs["opla"]["categorization"], pfs["rdfs"]["range"], pfs["xsd"]["string"]) )
+    graph.add( (pfs["opla"]["categorization"], pfs["rdfs"]["domain"], pattern_uri))
+    graph.add( (pfs["opla"]["categorization"], pfs["rdfs"]["range"], category_uri) )
 
 
     html_counter=1
@@ -98,10 +96,9 @@ def generate_index():
         graph.add( (noun_pattern_uri, pfs["opla"]["owlRepresentation"], Literal(f"{ttl_path}", datatype=XSD.string)) )
         graph.add( (noun_pattern_uri, pfs["rdfs"]["label"], Literal(f"{noun}", lang="en")) )
         graph.add( (noun_pattern_uri, pfs["opla"]["renderedSchemaDiagram"], Literal(f"{schema_path}", datatype=XSD.string)) )
-        graph.add( (noun_pattern_uri, pfs["rdfs"]["label"], Literal(f"{noun}", lang="en")) )
 
-        graph.add( (noun_pattern_uri, pfs["opla"]["categorization"], Literal(f"{ttl_path}", datatype=XSD.string)) )
-        graph.add( (noun_pattern_uri, pfs["rdfs"]["label"], Literal("Category", lang="en")) )
+        # graph.add( (noun_pattern_uri, pfs["opla"]["categorization"], Literal(f"{ttl_path}", datatype=XSD.string)) )
+        # graph.add( (noun_pattern_uri, pfs["rdfs"]["label"], Literal("Category", lang="en")) )
         
         noun_html = f'''
             <html>
@@ -116,6 +113,7 @@ def generate_index():
         graph.add( (noun_pattern_uri, pfs["opla"]["htmlDocumentation"], Literal(f"{noun_html}", datatype=XSD.string)) )
         graph.add( (noun_pattern_uri, pfs["rdfs"]["label"], Literal(f"{noun}", lang="en")) )
         html_counter+=1
+
     output_name = "csmodl.owl"
     output_path = os.path.join("../csmodl", output_name)
     graph.serialize(format="turtle", encoding="utf-8", destination=output_path)
